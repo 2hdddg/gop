1f04461068ebce10b7d95e8be863c6323ce7f26c
Peter Wilhelmsson <2hdddg@gmail.com>
2019-03-24 14:55:10 +0100

    Changed cmdline format

--- main.go
+++ main.go
@@ -2,42 +2,55 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/2hdddg/gop/client"
 	"github.com/2hdddg/gop/config"
 	"github.com/2hdddg/gop/server"
 )
 
-var (
-	isServer bool
-	port     int
-	params   client.Params
-)
+func serve() {
+	var port int
 
-func setupParameters() {
-	flag.BoolVar(&isServer, "serve", false, "Run server")
-	flag.IntVar(&port, "port", 8080, "Server port")
+	flags := flag.NewFlagSet("serve", flag.ExitOnError)
+	flags.IntVar(&port, "port", 8080, "Server port")
+	flags.Parse(os.Args[2:])
 
-	flag.StringVar(&params.Name, "name", "", "Find function")
-	flag.StringVar(&params.FilePath, "file", "", "Go file")
+	config := config.NewConfig()
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	server.Run(config, port)
+}
 
-	flag.Parse()
+func search() {
+	var (
+		port   int
+		params client.Params
+	)
+
+	flags := flag.NewFlagSet("search", flag.ExitOnError)
+	flags.IntVar(&port, "port", 8080, "Server port")
+	flags.StringVar(&params.Name, "name", "", "Find function")
+	flags.StringVar(&params.FilePath, "file", "", "Go file")
+	flags.Parse(os.Args[2:])
+
+	config := config.NewConfig()
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	client.Run(config, port, &params)
 }
 
 func main() {
-	setupParameters()
-	config := config.NewConfig()
-
-	// Configure standard logger
-	log.SetFlags(log.Ltime | log.Lshortfile)
-
-	if isServer {
-		server.Run(config, port)
+	command := os.Args[1]
+	switch command {
+	case "serve":
+		serve()
+	case "search":
+		search()
+	default:
+		fmt.Println("Illegal command")
 		return
 	}
-
-	client.Run(config, port, &params)
 }
 
 /*
